12ea2044efe566bce95423f6bd3894b9896c0ad3
HEMANTH <104354484+HEMANTH-1508@users.noreply.github.com>
2023-10-04 13:44:10 +0530

    Create arithematic.cpp

---
+++ arithematic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b,c;
+  cout<<"ENTER THREE NUMBERS : ";
+  cin>>a>>b>>c;
+  cout<<"\nSUM OF THREE NUMBERS IS : "<<a+b+c;
+  cout<<"\nDIFFERENC BETWEEN THREE NUMBERS IS : "<<a-b-c;
+  cout<<"\nMULTIPLICATION BETWEEN THREE NUMBERS IS : "<<a*b*c;
+  cout<<"\nDIVISION OF THREE NUMBERS IS : "<<a/b/c;
+  cout<<"\nMODULE OF THREE NUMBERS IS : "<<a%b%c;
+}
