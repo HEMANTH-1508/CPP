0c5a4e288021685e9bf64b3c0c4e0949d2c018f3
HEMANTH <104354484+HEMANTH-1508@users.noreply.github.com>
2023-10-04 13:43:26 +0530

    Create floating point.cpp

---
+++ floating point.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  float a,b,sum,sub,mul,div;
+  cout<<"\nENTER TWO NUMBERS : ";
+  cin>>a>>b;
+  sum=a+b;
+  sub=a-b;
+  mul=a*b;
+  div=a/b;
+  cout<<"\nADDITION : "<<sum;
+  cout<<"\nSUBRACTION : "<<sub;
+  cout<<"\nMULTIPLICATION : "<<mul;
+  cout<<"\nDIVISION : "<<div;
+}
